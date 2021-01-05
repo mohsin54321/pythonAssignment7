56f6d16bf069996a3722d535b5d9b1873da42d1a
ChuaKaiMing <73737339+ChuaKaiMing@users.noreply.github.com>
2021-01-05 09:10:36 +0800

    Add files via upload
    This is the updated version.

--- testcodes.py
+++ testcodes.py
@@ -7,13 +7,13 @@ for a in AllVar:
     songdictionary[a] = EachVarValue[counterforlist2]
     counterforlist2 += 1
 
-print('Hello. Welcome!')
-#Until here the code is working absolutely fine
+print('Hello, Welcome!')
+
 for b in AllVar:
-    if b == AllVar[1,4]:
+    if b == AllVar[1:4]:
         print('Do you want to guess what is the', AllVar[b] + '?')
         reply = str(input('Yes or No? 5 chances for each questions. Type right down here\n'))
-        if reply == Yes or reply == yes:
+        if reply == 'Yes' or reply == 'yes':
             for c in range(1,6):
                 answer = str(input('Chance {} / Your answer: '.format(c)))
                 if answer == EachVarValue[1] or answer == EachVarValue[2] or answer == EachVarValue[3]:
@@ -22,12 +22,13 @@ for b in AllVar:
                     print('The answer is:',EachVarValue[b])
                 else:
                     print('Incorrect answer.')
-        elif reply == No or reply == no:
-            print(AllVar[0,b+1])
+        elif reply == 'No' or reply == 'no':
+            print(AllVar[0:b+1])
         else:
             reply2 = str(input('Skip all?\n'))
-            if reply2 == skip all or reply2 == Skip all:
+            if reply2 == 'skip all' or reply2 == 'Skip all':
                 print(songdictionary)
+                #Until here the code is working absolutely fine
     else:
-        print(EachVarValue[0,b])
-print(songdictionary)       
+        print(EachVarValue[0:b])
+print(songdictionary)
